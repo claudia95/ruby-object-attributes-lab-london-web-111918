7393ab15cac74e2ddb3001d8c8c567325fb64ead
Claudia Fox <claudiafox95@icloud.com>
2018-11-20 21:02:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,11 @@
 
 class Dog
   
-  def initialize(name)
-    @name = name
-  end
+  def name 
   def name=(name)
     @name = name
   end
-  def initialize(breed)
-    @breed = breed
-  end
+ 
   def breed=(breed)
     @breed = breed
   end
